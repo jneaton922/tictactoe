9e3d43a80277239dd69b5ff6f6d0ee40d05caa31
UnwaryBuffalo <jneaton922@gmail.com>
2021-12-08 16:07:34 -0500

    initial repo structure, minimal progress

---
+++ scripts/tictactoe.js
@@ -0,0 +1,26 @@
+
+
+const Player = function(name,symbol)
+{
+    const get_name = () => name;
+    const get_symbol = () => symbol;
+
+}
+
+const GameBoard = (
+    function () 
+    {
+        let board = [['','','',''],['','','']['','','']];
+    }
+)();
+
+const DisplayController = (
+    function ()
+    {
+        const update = function(game_board)
+        {
+            
+        }
+    }
+)();
+
